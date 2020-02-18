e64f0b0c08dc47c9bfd0e099b7c6075bb58a22b1
Joe Ellingworth <joeellingworth@gmail.com>
2020-02-18 18:00:34 +0000

    first three user stories complete

--- spec/AirportSpec.js
+++ spec/AirportSpec.js
@@ -1,15 +1,27 @@
 describe("Airport", function() {
-  
+  var plane
+  var airport
+  beforeEach(function() {
+    plane = new Plane(); 
+    airport = new Airport();
+  });
+
   it('should let a plane land', function(){
-    var plane = new Plane(); 
-    var airport = new Airport();
-    expect(airport.land(plane)).toEqual[plane]
+    airport.land(plane)
+    expect(airport.storage).toEqual([plane])
   });
 
   it('plane should have taken off', function(){
-    var plane = new Plane(); 
-    var airport = new Airport();
-    expect(airport.land(plane)).toEqual[plane]
+    airport.land(plane)
+    airport.takeoff(plane)
+    expect(airport.storage).toEqual([])
+  });
+
+  it('plane should have been prevented from landing due to capacity', function(){
+    for(var i=0; i < airport.capacity; i++){
+    airport.land(plane);
+    }
+    expect(function(){airport.land(plane)}).toThrowError("Airport is full");
   });
 
 });
--- src/Airport.js
+++ src/Airport.js
@@ -1,7 +1,16 @@
 function Airport() {
   this.storage = []
+  this.capacity = 5
 }
 
 Airport.prototype.land = function(plane) {
-this.storage.push(plane)
+  if (this.storage.length >= this.capacity) {
+    throw new Error("Airport is full")
+  } else {
+    this.storage.push(plane)
+  }
+}
+
+Airport.prototype.takeoff = function() {
+  this.storage.pop()
 }
